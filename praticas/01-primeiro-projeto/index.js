78329bbbf20f9b53158206f14dbf8c681cc4fbd7
math45rosa-crypto <Matheus45rosa@gmail.com>
2025-08-20 21:32:36 -0300

    Criando meu primeiro projeto

---
+++ praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,22 @@
+console.log("Rodando meu priomeiro projeto em NODE")
+
+
+let nome = "Matheus"
+
+let sobrenome = "Rosa"
+
+console.log(nome + " " + sobrenome)
+
+let idade = 22
+
+console.log("A idade do  " + nome + " é " + idade)
+
+let exercicios = 0.5
+
+let trabalho = 1.5
+
+let prova = 6
+
+let notaA1Calculada = exercicios + trabalho + prova
+
+console.log("NOTA A1: " + notaA1Calculada)
